8a46c362ffca1518a9aba9fb47a2c94e4613b209
Daniel Myers <daniel.neil.myers@gmail.com>
2023-06-06 16:54:41 -0500

    Commit codecademy project Video Player. Make three React components work together to create a responsive video player.

---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Menu = ({ onSelectVideo }) => {
+    const clickHandler = ({ target }) => {
+        const name = target.value;
+        onSelectVideo(name);
+    };
+
+    return (
+        <form onClick={clickHandler}>
+            <input type="radio" name="src" value="fast" /> fast
+            <input type="radio" name="src" value="slow" /> slow
+            <input type="radio" name="src" value="cute" /> cute
+            <input type="radio" name="src" value="eek" /> eek
+        </form>
+    );
+};
+
+export default Menu;
